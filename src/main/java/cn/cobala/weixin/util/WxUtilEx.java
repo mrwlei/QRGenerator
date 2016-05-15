0d859a57f71a118eae836fc88ebf8e9104a50838
mrwlei <532362868@qq.com>
2016-05-15 23:40:35 +0800

    微信APP支付 和 微信扫码支付的工具类

---
+++ src/main/java/cn/cobala/weixin/util/WxUtilEx.java
@@ -0,0 +1,276 @@
+package cn.cobala.weixin.util;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.input.SAXBuilder;
+import org.jdom.output.Format;
+import org.jdom.output.XMLOutputter;
+
+import cn.cobala.weixin.client.TenpayHttpClient;
+
+
+public class WxUtilEx {
+
+	public static final String CHARSET = "UTF-8";
+	
+	private static final String POST_URL_UNIFIEDORDER = "https://api.mch.weixin.qq.com/pay/unifiedorder";
+	private static final String POST_URL_ORDERQUERY = "https://api.mch.weixin.qq.com/pay/orderquery";
+	
+
+	/*
+	private static final String POST_URL_UNIFIEDORDER = "http://localhost:8808/bytsp/mobile_test.jsp?stub=unifiedorder";
+	private static final String POST_URL_ORDERQUERY = "http://localhost:8808/bytsp/mobile_test.jsp?stub=orderquery";
+	*/
+
+	
+	public static void formatMapObject(Map map) {
+
+		Set es = map.entrySet();
+		Iterator it = es.iterator();
+		List<String> remove_key_list = new ArrayList<String>();
+
+		while (it.hasNext()) {
+			Map.Entry entry = (Map.Entry) it.next();
+			String k = (String) entry.getKey();
+			String v = (String) entry.getValue();
+
+			if (null == v || v.length() < 1) {
+				remove_key_list.add(k);
+			}
+		}
+		
+		for(String key : remove_key_list){
+			map.remove(key);
+		}
+		
+	}
+	
+	public static Map xmlString2Map(String xml) throws Exception {
+		Map map = new HashMap();
+
+		if (null == xml || xml.trim().length() < 1) {
+			return map;
+		}
+
+		ByteArrayInputStream bais = new ByteArrayInputStream(xml.getBytes(CHARSET));
+		SAXBuilder builder = new SAXBuilder();
+		Document doc = builder.build(bais);
+		Element root = doc.getRootElement();
+		List list = root.getChildren();
+		Iterator it = list.iterator();
+
+		Element e = null;
+		String k = null;
+		String v = null;
+
+		while (it.hasNext()) {
+			e = (Element) it.next();
+			k = e.getName();
+			v = e.getTextNormalize();
+
+			map.put(k, v);
+		}
+
+		return map;
+	}
+
+	public static String map2QueryString(Map map) {
+
+		StringBuffer sb = new StringBuffer();
+
+		SortedMap<String, String> temp_map = new TreeMap<String, String>(map);
+		temp_map.remove("key");
+		temp_map.remove("sign");
+
+		Set es = temp_map.entrySet();
+		Iterator it = es.iterator();
+
+		while (it.hasNext()) {
+			Map.Entry entry = (Map.Entry) it.next();
+			String k = (String) entry.getKey();
+			String v = (String) entry.getValue();
+
+			if (null == v || v.length() < 1) {
+				continue;
+			}
+
+			if (sb.length() > 0) {
+				sb.append("&");
+			}
+
+			sb.append(k);
+			sb.append("=");
+			sb.append(v);
+		}
+
+		return sb.toString();
+	}
+
+	public static Map requestStream2Map(HttpServletRequest request) throws Exception {
+		String str = requestStream2String(request);
+		return xmlString2Map(str);
+	}
+
+	public static String requestStream2String(HttpServletRequest request) throws Exception {
+		String data = (String) request.getAttribute("request-input-stream");
+
+		System.out.println(">>>>>>> requestStream2String\n" + data);
+		return data;
+	}
+
+	private static String ___requestStream2String___(HttpServletRequest request) throws Exception {
+
+		InputStream is = request.getInputStream();
+		String charset = request.getCharacterEncoding();
+		BufferedReader br = new BufferedReader(new InputStreamReader(is, charset));
+
+		StringBuffer sb = new StringBuffer();
+		String line = br.readLine();
+		while (line != null) {
+			sb.append(line);
+			sb.append("\r\n");
+
+			line = br.readLine();
+		}
+
+		br.close();
+		is.close();
+
+		return sb.toString();
+	}
+
+	public static String map2XmlString(Map map) throws Exception {
+
+		Element root = new Element("xml");
+		Document document = new Document(root);
+
+		Set es = map.entrySet();
+		Iterator it = es.iterator();
+		Map.Entry entry = null;
+		String k = null;
+		String v = null;
+		Element elements = null;
+
+		while (it.hasNext()) {
+			entry = (Map.Entry) it.next();
+
+			k = (String) entry.getKey();
+			v = (String) entry.getValue();
+
+			elements = new Element(k);
+			elements.setText(v);
+
+			root.addContent(elements);
+		}
+
+		Format format = Format.getPrettyFormat();
+		format.setEncoding(CHARSET);//设置xml文件的字符为UTF-8，解决中文问题
+		XMLOutputter xmlout = new XMLOutputter(format);
+		ByteArrayOutputStream bo = new ByteArrayOutputStream();
+		xmlout.output(document, bo);
+
+		return bo.toString(CHARSET);
+	}
+
+	public static String makeSignWithMd5(String data, String key) {
+		data = data + "&key=" + key;
+		String sign = MD5Util.MD5Encode(data, CHARSET).toUpperCase();
+
+		System.out.println("\n>>>>>>>>>> raw String \n" + data);
+		
+		return sign;
+	}
+
+	public static String makeSignWithMd5(Map map, String key) {
+
+		String data = map2QueryString(map);
+		String sign = makeSignWithMd5(data, key);
+
+		return sign;
+	}
+
+	public static void signMapWithMd5(Map map, String key) {
+
+		formatMapObject(map);
+		String sign = makeSignWithMd5(map, key);
+
+		map.remove("key");//防止key被存储
+		map.put("sign", sign);
+	}
+
+	public static boolean compareSign(String signA, String signB) {
+		if (signA != null && signB != null && signA.equals(signB)) {
+			return true;
+		}
+		return false;
+		//return true;
+	}
+
+	public static boolean checkMapSign(Map map, String key) {
+
+		String old_sign = (String) map.get("sign");
+		String new_sign = makeSignWithMd5(map, key);
+
+		return compareSign(new_sign, old_sign);
+	}
+
+	public static Map postUnifiedOrder(Map map) throws Exception {
+		return postXmlData(POST_URL_UNIFIEDORDER, map);
+	}
+
+	public static Map postOrderQuery(Map map) throws Exception {
+		return postXmlData(POST_URL_ORDERQUERY, map);
+	}
+
+	public static Map postXmlData(String url, Map map) throws Exception {
+
+		Map<String, String> return_map = new HashMap();
+		
+		TenpayHttpClient thc = new TenpayHttpClient();
+		Field field = TenpayHttpClient.class.getDeclaredField("charset");
+		field.setAccessible(true);
+		field.set(thc, CHARSET);
+		String post_data = map2XmlString(map);
+		
+		System.out.println("\n>>>>>> post_data \n" + post_data);
+
+		if (thc.callHttpPost(url, post_data)) {
+			String return_data = thc.getResContent();
+			
+			System.out.println("\n>>>>>> return_data \n" + return_data);
+			
+			return_map = xmlString2Map(return_data);
+		} else {
+			throw new Exception("postXmlData error!!! " + url);
+		}
+
+		return return_map;
+	}
+
+	public static String getCurrentTime() {
+		return TenpayUtil.getCurrTime();
+	}
+
+	public static String getTimeStamp() {
+		return String.valueOf(TenpayUtil.getUnixTime(new Date()));
+	}
+
+}
---
+++ src/test/java/cn/cobala/weixin/PayTest.java
@@ -0,0 +1,121 @@
+package cn.cobala.weixin;
+
+import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+import org.apache.commons.lang.StringUtils;
+import org.junit.Test;
+
+
+import cn.cobala.weixin.util.WxUtilEx;
+
+
+public class PayTest {
+	
+	// 模拟统一下单
+	@Test
+	public void testUnifyOrder() throws Exception {
+		String appid = "";                    //公众号ID     微信分配的公众账号ID
+		String mch_id = "";                    //商户号              微信支付分配的商户号 
+		String device_info = "";                               //设备号              微信支付分配的终端设备号，商户自定义 
+		String nonce_str = "randomUUID()";              //随机字符串       随机字符串，不长于32位          
+		String body = "";                 //商品描述            商品或支付单简要描述
+		String detail = "";             //商品详情           商品名称明细列表
+		String attach = "";                                    //附加数据            附加数据，在查询API和支付通知中原样返回，该字段主要用于商户携带订单的自定义数据 
+		String out_trade_no = "";      //商户订单号        商户系统内部的订单号,32个字符内、可包含字母, 其他说明见商户订单号
+		String fee_type = "CNY";                               //货币类型            符合ISO 4217标准的三位字母代码，默认人民币：CNY，其他值列表详见货币类型
+		String total_fee = "";   //总金额                订单总金额，只能为整数，详见支付金额
+		String spbill_create_ip = "";      //终端IP        APP和网页支付提交用户端ip，Native支付填调用微信支付API的机器IP
+		String time_start = WxUtilEx.getCurrentTime();          //交易起始时间     订单生成时间，格式为yyyyMMddHHmmss，如2009年12月25日9点10分10秒表示为20091225091010
+		String time_expire = "";                               //交易结束时间     订单失效时间，格式为yyyyMMddHHmmss，如2009年12月27日9点10分10秒表示为20091227091010
+		String goods_tag = "";                                 //商品标记             商品标记，代金券或立减优惠功能的参数，说明详见代金券或立减优惠
+		String notify_url = "";  //通知地址             接收微信支付异步通知回调地址
+		String trade_type = "APP";                             //交易类型             取值如下：JSAPI，NATIVE，APP
+		String product_id = "";                                //商品id        trade_type=NATIVE，此参数必传。此id为二维码中包含的商品ID，商户自行定义
+		String openid = "";                                    //用户标识             trade_type=JSAPI，此参数必传，用户在商户appid下的唯一标识。下单前需要调用【网页授权获取用户信息】接口获取到用户的Openid
+		String key = "";  //微信appkey
+		
+		
+		//=========================
+		//生成预支付单
+		//=========================
+
+		//设置预支付单 请求参数
+		SortedMap<String, String> post_map = new TreeMap<String, String>();
+		post_map.put("appid", appid);
+		post_map.put("mch_id", mch_id);
+		post_map.put("device_info", device_info);
+		post_map.put("nonce_str", nonce_str);
+		post_map.put("body", body);
+		post_map.put("detail", detail);
+		post_map.put("attach", attach);
+		post_map.put("out_trade_no", out_trade_no);
+		post_map.put("fee_type", fee_type);
+		post_map.put("total_fee", total_fee);
+		post_map.put("spbill_create_ip", spbill_create_ip);
+		post_map.put("time_start", time_start);
+		post_map.put("time_expire", time_expire);
+		post_map.put("goods_tag", goods_tag);
+		post_map.put("notify_url", notify_url);
+		post_map.put("trade_type", trade_type);
+		post_map.put("product_id", product_id);
+		post_map.put("openid", openid);
+		
+		
+		//签名
+		WxUtilEx.signMapWithMd5(post_map, key);
+				
+		//发送请求，获取预支付信息
+		Map<String, String> return_map = WxUtilEx.postUnifiedOrder(post_map);
+		
+		
+		//-----------------统一支付返回结果----------------
+		if ("SUCCESS".equals(return_map.get("return_code"))) {
+			//TODO
+			
+		}
+				
+		if ("SUCCESS".equals(return_map.get("return_code")) && "SUCCESS".equals(return_map.get("result_code"))) {
+			//TODO
+			
+		}
+		
+		if (!WxUtilEx.checkMapSign(return_map, key)) {
+			throw new Exception("错误：签名不相同");
+		}
+		//---------------统一支付返回结果结束-----------------
+		
+		
+		//------------------------扫码支付-------------------------------
+		//如果是扫码支付，则只关注code_url即可
+		String coed_url = return_map.get("code_url"); //微信二维码URL
+		//TODO 
+		//return code url
+		//------------------------扫码支付结束-------------------------------
+		
+		//------------------------APP端支付返回的数据-------------------------------
+		//如果是APP端，返回给APP数据
+		SortedMap<String, String> app_call_pack = new TreeMap<String, String>();
+		
+		//获取预支付ID ， 此预支付ID为APP端调用
+		String prepayid = return_map.get("prepay_id");
+		if (StringUtils.isNotBlank(prepayid)) { 
+			//设置支付参数
+			app_call_pack.put("appid", appid); //公众号id    微信分配的公众账号ID
+			app_call_pack.put("partnerid", mch_id); //商户号            微信支付分配的商户号
+			app_call_pack.put("prepayid", prepayid); //预支付交易会话id    微信返回的支付交易会话ID
+			app_call_pack.put("package", "Sign=WXPay"); //扩展字段              暂填写固定值Sign=WXPay
+			app_call_pack.put("noncestr", "randomUUID()"); //随机字符串, 不长于32位
+			app_call_pack.put("timestamp", WxUtilEx.getTimeStamp()); //时间戳
+			
+			//签名
+			WxUtilEx.signMapWithMd5(app_call_pack, key);
+			
+			//返回数据给APP端
+			//TODO 
+			// reutrn app_call_pack
+		}
+		//------------------------APP端支付返回的数据结束-------------------------------
+	}
+}
