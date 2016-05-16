44b484103620abe0bb333a656790274bd65652ea
王雷 <532362868@qq.com>
2016-05-16 21:04:12 +0800

    二维码生成类

--- src/main/java/cn/cobala/qr/generator/util/LogoConfig.java
+++ src/main/java/cn/cobala/qr/generator/util/LogoConfig.java
@@ -5,7 +5,6 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.geom.RoundRectangle2D;
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
