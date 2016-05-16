727134e2eae3382ca540ccab03debb9078a13531
王雷 <532362868@qq.com>
2016-05-16 16:02:06 +0800

    Test

--- src/test/java/cn/cobala/qr/generator/QRGeneratorTest.java
+++ src/test/java/cn/cobala/qr/generator/QRGeneratorTest.java
@@ -1,8 +1,12 @@
 package cn.cobala.qr.generator;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.junit.Test;
 
@@ -22,9 +26,22 @@ public class QRGeneratorTest {
 //		qg.generateQRToFile(new File("F://aaa.png"));
 		
 		
-		QRGenerator qg = new QRGenerator("扫一下，就会出现此内容偶。并且二维码中间还会有自己的logo,logo图片应放在classpath下",
-										"png", 150, 150, "UTF-8", "/logo.jpg");
+//		QRGenerator qg = new QRGenerator("扫一下，就会出现此内容偶。并且二维码中间还会有自己的logo,logo图片应放在classpath下",
+//										"png", 150, 150, "UTF-8", "/logo.jpg");
+//		qg.generateQRToFile(new File("F://aaa.png"));
+		
+		QRGenerator qg = new QRGenerator("扫一下，就会出现此内容偶。",260,260);
 		qg.generateQRToFile(new File("F://aaa.png"));
+		
+		/*返回字节数组示例*/
+//		QRGenerator qg = new QRGenerator("扫一下，就会出现此内容偶。",260,260);
+//		byte[] qrBytes = qg.generateQRToByteAraay();
+		
+		/*以输出流形式示例*/
+//		QRGenerator qg = new QRGenerator("扫一下，就会出现此内容偶。",260,260);
+//		HttpServletResponse response = null;
+//		qg.generateQRToOutputStream(response.getOutputStream());
+		
 		
 	}
 }
